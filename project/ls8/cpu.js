7118e3c58dd4c6dc219199a702df2716091d3ab0
Jonathan Bry <jonathanbry@comcast.net>
2018-04-13 13:17:34 -0700

    final commit

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -6,26 +6,18 @@
  * Class for simulating a simple Computer (CPU & memory)
  */
 class CPU {
-
-
-
     /**
      * Initialize the CPU
      */
     constructor(ram) {
+        this.flags = {
+            equal: false
+        };
         this.ram = ram;
-        this.SP = 0xF4;
         this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
-
+        this.SP = 0xf4;
         // Special-purpose registers
         this.reg.PC = 0; // Program Counter
-    }
-
-    ValueSP() { // get value of stack pointer
-       if (this.ram[0xF3] != 0) {
-            return this.ram[0xF3];
-       }
-       return this.ram[0xF4];
     }
 
     /**
@@ -34,7 +26,6 @@ class CPU {
     poke(address, value) {
         this.ram.write(address, value);
     }
-
 
     /**
      * Starts the clock ticking on the CPU
@@ -67,9 +58,13 @@ class CPU {
             case 'MUL':
                 const indexA = parseInt(regA, 2);
                 const indexB = parseInt(regB, 2);
-                this.reg[indexA] = (this.reg[indexA] * this.reg[indexB]);
+                this.reg[indexA] = this.reg[indexA] * this.reg[indexB];
                 // !!! IMPLEMENT ME
                 break;
+            case 'CMP':
+                if ( registerA === registerB) {
+                       this.flags.equal = true;
+                }   // this.flags[equal] = (valA === valB);
         }
     }
 
@@ -81,13 +76,13 @@ class CPU {
         // from the memory address pointed to by the PC. (I.e. the PC holds the
         // index into memory of the instruction that's about to be executed
         // right now.)
-
+        this.reg[7] = this.SP;
         const IR = this.ram.read(this.reg.PC); // binary number
 
         // !!! IMPLEMENT ME
 
         // Debugging output
-        // console.log(`${this.reg.PC}: ${IR.toString(2)}`);
+        //
 
         // Get the two bytes in memory _after_ the PC in case the instruction
         // needs them.
@@ -113,10 +108,23 @@ class CPU {
                 break;
             case '01001100':
                 this.POP(operandA);
+                break;
             case '01001101':
                 this.PUSH(operandA);
+                break;
+            case '01001000':
+                this.CALL(operandA);
+                break;
+            case '00001001':
+                this.RET();
+                break;
+            case '10101000':
+                this.ADD(operandA, operandB);
+                break;
+            case '10100000':
+                this.CMP(operandA, operandB);
+                break;
             default:
-                console.log('instruction code not found');
         }
 
         // !!! IMPLEMENT ME
@@ -127,7 +135,6 @@ class CPU {
         // for any particular instruction.
         this.reg.PC = this.reg.PC + parseInt(IR.slice(0, 2), 2) + 1;
     }
-
 
     PRN(register) {
         const index = parseInt(register, 2);
@@ -144,14 +151,50 @@ class CPU {
     }
 
     PUSH(register) {
-        // this.ram[this.SP - 1] = this.ram[this.SP];
-        // this.SP = 0xF3;
-        // this.ram[SP] = this.reg[register];
+        const index = parseInt(register, 2);
+        this.SP--;
+        this.ram[this.SP] = this.reg[index];
+    }
+
+    _push(value) {
+        this.SP--;
+        this.ram[this.SP] = value;
     }
 
     POP(register) {
-        this.reg[register] = this.ram[this.SP]
+        const index = parseInt(register, 2);
+        this.reg[index] = this.ram[this.SP];
+        this.SP++;
     }
+
+    _pop() {
+        const value = this.ram[this.SP];
+        this.SP++;
+        return value;
+    }
+
+    CALL(register) {
+        this._push(this.reg.PC);
+        const index = parseInt(register, 2);
+        this.reg.PC = parseInt(this.reg[index], 2) - 2;
+    }
+
+    RET() {
+        this.reg.PC = this._pop() + 1;
+    }
+
+    ADD(registerA, registerB) {
+        const indexA = parseInt(registerA, 2);
+        const indexB = parseInt(registerB, 2);
+        this.reg[indexA] = (
+            parseInt(this.reg[indexA], 2) + parseInt(this.reg[indexB], 2)
+        ).toString(2);
+    }
+    CMP(registerA, registerB) {
+            if ( registerA === registerB) {
+                   this.flags.equal = true;
+            }   // this.flags[equal] = (valA === valB);
+    }     
 }
 
 module.exports = CPU;
